ca7a750d27aa390a4316369da74004291716ae59
Altair5869 <th0538@naver.com>
2025-03-07 16:46:41 +0900

    Divide

--- src/main/java/dev/friox/Main.java
+++ src/main/java/dev/friox/Main.java
@@ -1,7 +1,20 @@
 package dev.friox;
 
+class Div{
+    private int a,b;
+
+    public Div(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int divide(int a,int b) {
+        return a/b;
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("===============계산기============");
     }
+
 }
