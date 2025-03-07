008ccc64397edab1807ca81c5cd3f78f5ca9c241
jam <jjam9582@naver.com>
2025-03-07 16:17:47 +0900

    권재은

--- src/main/java/dev/friox/Main.java
+++ src/main/java/dev/friox/Main.java
@@ -2,6 +2,6 @@ package dev.friox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("이번엔 되려나");
     }
 }
