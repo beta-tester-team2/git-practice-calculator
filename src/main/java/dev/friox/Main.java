673d4557c5079075631f31cc1cc36fe4f842a517
jam <jjam9582@naver.com>
2025-03-07 17:01:44 +0900

    최종 계산기

--- src/main/java/dev/friox/Main.java
+++ src/main/java/dev/friox/Main.java
@@ -1,4 +1,5 @@
 package dev.friox;
+import java.util.Scanner;
 
 class Sum {
     private int a, b, sum;
@@ -26,7 +27,22 @@ class Div{
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("===============계산기============");
+        System.out.println("===============계산기==============");
+        System.out.println("계산할 값을 2개 입력해주세요: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        Sum sum = new Sum();
+        Div div = new Div(a,b);
+        Minus minus = new Minus();
+        Mulitplication mulitplication = new Mulitplication();
+
+        System.out.println(a+" 와 "+b+" 를 더한 값은 "+sum.Sum(a,b)+" 입니다.");
+        System.out.println(a+" 와 "+b+" 를 뺀 값은 "+minus.execute(a, b)+" 입니다.");
+        System.out.println(a+" 와 "+b+" 를 곱한 값은 "+ mulitplication.multiply(a,b)+" 입니다.");
+        System.out.println(a+" 와 "+b+" 를 나눈 값은 "+div.divide(a,b)+" 입니다.");
+
     }
 }
 
